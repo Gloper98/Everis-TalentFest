6aca5224a6ad74083f3407ef812ba9febb6b32ae
NelidaSh <nelida.quispetacuri@gmail.com>
2018-04-11 15:19:04 -0500

    footer

--- src/app/footer/footer.module.ts
+++ src/app/footer/footer.module.ts
@@ -1,18 +1,15 @@
 import { NgModule } from '@angular/core';
-import { HomeComponent } from './home.component';
 import { FooterComponent } from '../footer/footer.component';
-
 
 @NgModule({
     declarations: [
-        HomeComponent,
         FooterComponent
     ],
     exports: [
-        HomeComponent
+        FooterComponent
     ]
 })
 
-export class HomeModule {
+export class FooterModule {
 
 }
