3e4254ceb1efaa95b6565219df6ad0ba4d00359f
NelidaSh <nelida.quispetacuri@gmail.com>
2018-04-12 01:36:43 -0500

    modificando login

--- src/app/home/main/myProyects/myProyects.component.ts
+++ src/app/home/main/myProyects/myProyects.component.ts
@@ -1,20 +1,3 @@
-// import { Component } from '@angular/core';
-
-// @Component({
-//     selector: 'app-my-proyects',
-//     templateUrl: './myProyects.component.html',
-//     styleUrls: ['./myProyects.component.css']
-// })
-
-// export class MyProyectsComponent {
-//     public title: string;
-
-//     constructor(){
-//         this.title = 'proyectos';
-//     }
-
-// }
-
 import { Component, OnInit } from '@angular/core';
 import { MyProyectsService } from './myProyects.service';
 
